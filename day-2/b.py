29bc422338eba8a0a5a4daf01c6377c1b20b4e59
Victor Cid <52244754+Vleyked@users.noreply.github.com>
2022-12-02 22:25:49 +0100

    day-2

---
+++ day-2/b.py
@@ -0,0 +1,66 @@
+def main(input_file):
+    """Prints the answer to the first part of the second day's puzzle.
+    Args:
+        input_file (str): The path to the input file.
+
+    Returns:
+        None
+    """
+    # read input
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+
+    # parse input
+    rounds = []
+    for line in lines:
+        rounds.append(line.strip().split())
+
+    # selection score
+    score_values = {"rock": 1, "paper": 2, "scissors": 3}  # A, B, C
+    # win/loss/draw score
+    win_loss_draw = {"win": 6, "loss": 0, "draw": 3}  # Y, X, Z
+    score = 0
+
+    # calculate score
+    for round in rounds:
+        # Your opponent (round[0]) has chosen rock (A)
+        if round[0] == "A":
+            # You should draw (Y)
+            if round[1] == "Y":
+                score += score_values["rock"] + win_loss_draw["draw"]
+            # You should loose (X)
+            if round[1] == "X":
+                score += score_values["scissors"] + win_loss_draw["loss"]
+            # You should win (Z)
+            if round[1] == "Z":
+                score += score_values["paper"] + win_loss_draw["win"]
+
+        # Your opponent (round[0]) has chosen paper (B)
+        if round[0] == "B":
+            # You should draw (Y)
+            if round[1] == "Y":
+                score += score_values["paper"] + win_loss_draw["draw"]
+            # You should loose (X)
+            if round[1] == "X":
+                score += score_values["rock"] + win_loss_draw["loss"]
+            # You should win (Z)
+            if round[1] == "Z":
+                score += score_values["scissors"] + win_loss_draw["win"]
+
+        # Your opponent (round[0]) has chosen scissors (C)
+        if round[0] == "C":
+            # You should draw (Y)
+            if round[1] == "Y":
+                score += score_values["scissors"] + win_loss_draw["draw"]
+            # You should loose (X)
+            if round[1] == "X":
+                score += score_values["paper"] + win_loss_draw["loss"]
+            # You should win (Z)
+            if round[1] == "Z":
+                score += score_values["rock"] + win_loss_draw["win"]
+
+    print(score)
+
+
+if __name__ == "__main__":
+    main("day-2/input.txt")
